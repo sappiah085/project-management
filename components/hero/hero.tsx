7df4ce8a9d09c394956e0fac73ddec2f14b66e12
realmann <sappiah085@st.ug.edu.gh>
2023-02-25 16:10:39 +0100

    added animation loader

--- components/hero/hero.tsx
+++ components/hero/hero.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
 import hero from "../../public/assets/image.png";
 import img2 from "../../public/assets/2.png";
 import img3 from "../../public/assets/3.png";
 import img4 from "../../public/assets/4.png";
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const land: { label: string; image: any }[] = [
   {
@@ -31,7 +31,8 @@ const land: { label: string; image: any }[] = [
 ];
 export default function Hero() {
   const [index, setIndex] = useState(0);
-  const selected = land[index];
+  const [width, setWidth] = useState(100);
+  const container: any = useRef(null);
   const numberVariant = {
     visible: (i: number) => ({
       opacity: 1,
@@ -42,6 +43,11 @@ export default function Hero() {
     }),
     hidden: { opacity: 0, y: 5 },
   };
+
+  useEffect(() => {
+    setWidth(container?.current?.clientWidth);
+  }, []);
+
   useEffect(() => {
     const timer = setInterval(() => {
       if (index < 3) {
@@ -56,20 +62,33 @@ export default function Hero() {
     };
   }, [index]);
   return (
-    <div className="h-screen w-full relative pt-9 left-0 top-0 flex flex-col gap-2 justify-center after:absolute after:w-full after:h-full after:bg-black/25 after:top-0 after:left-0  after:-z-20 transition-all">
-      <img
-        className="w-full h-full -z-20 object-cover absolute top-0 left-0 lg:pt-20"
-        src={selected.image.src}
-        alt="school boy"
-      />
-      <motion.h1
-        initial={{ opacity: 0, x: "-50px" }}
-        whileInView={{ opacity: 1, x: 0, transition: { duration: 1 } }}
-        className="z-[2] px-4  text-white font-sora text-3xl lg:text-5xl  font-bold md:w-[700px] lg:px-14"
-      >
-        {selected.label}
-      </motion.h1>
-      <div className="z-[2] flex items-center w-full px-4 gap-2 lg:px-14">
+    <div className="h-screen w-full relative flex flex-col justify-center overflow-hidden ">
+      {land.map(({ image, label }, id) => {
+        const trans = (id - index) * width;
+        console.log(trans);
+        return (
+          <span
+            ref={container}
+            key={label}
+            style={{ transform: `translateX(${trans}px)` }}
+            className={` h-full w-full absolute pt-9 left-0  top-0 flex flex-col gap-2 justify-center after:absolute after:w-full after:h-full after:bg-black/25 after:top-0 after:left-0  after:-z-20 transition-all`}
+          >
+            <img
+              className="w-full h-full -z-20 object-cover absolute top-0 left-0 lg:pt-20"
+              src={image.src}
+              alt="school boy"
+            />
+            <motion.h1
+              initial={{ opacity: 0, x: "-50px" }}
+              whileInView={{ opacity: 1, x: 0, transition: { duration: 1 } }}
+              className="z-[2] px-4  text-white font-sora text-3xl lg:text-5xl  font-bold md:w-[700px] lg:px-14"
+            >
+              {label}
+            </motion.h1>
+          </span>
+        );
+      })}
+      <div className="z-[2] flex items-center w-full px-4 gap-2 lg:px-14 mt-96">
         {[1, 2, 3, 4].map((number) => (
           <motion.button
             onClick={() => setIndex(number - 1)}
